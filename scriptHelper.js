4678c2e305a83060871585a0a6fa27ff380aa1c3
unknown <rads.veerapaneni@gmail.com>
2023-04-22 11:02:52 -0500

    changes

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,26 +2,46 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
+   /// Here is the HTML formatting for our mission target div.
+   
+              ` <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance} </li>
+                    <li>Number of Moons:${moons} </li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">`;
+   
 }
 
 function validateInput(testInput) {
+
+    let input = Number(testInput)
+    if ( input === ""){
+        return "Empty";
+    } else if (input = NaN){
+        return "Not a Number";
+    }else (input === true){
+        return "Is a Number";
+    }
+
    
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
+    let pilotStatus = document.getElementById("pilotStatus");
+    let copilotStatus = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus")
+    let cargoStatus = document.getElementById("cargoStatus");
+
+    if(validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty"){
+        alert("All fields are required!");
+    }else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === " Is not a Number" || validateInput(cargoLevele) === "Is not a Number"){
+        alert("Make sure to enter valid information for each field!")
+    }
 }
 
 async function myFetch() {
